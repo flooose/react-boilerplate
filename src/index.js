bd648815333ecf38ed164ddcb328c73615c37ccd
christopher floess <chris.floess@jjff.de>
2017-11-03 17:49:10 -0400

    Add support for css files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
-// import './index.css';
+import './index.css';
 
 export default class ShoppingList extends React.Component {
   render() {
